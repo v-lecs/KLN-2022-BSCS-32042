f7941693519ffc721226b389644a144517718fbf
chandulaed <chandulaed@gmail.com>
2022-03-27 04:19:18 +0530

    this commit will add single and multiple message client and server communication

--- chandula_007/uok-messenger/src/main/java/org/uok/dscc/App.java
+++ chandula_007/uok-messenger/src/main/java/org/uok/dscc/App.java
@@ -1,13 +1,54 @@
 package org.uok.dscc;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+
+
+public class App
 {
+
+    public static void clientcon(String message){
+        try{
+            Socket s= new Socket("127.0.0.1", 60000);
+            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+            dout.writeUTF(message);
+            if (message.equalsIgnoreCase("over")) {
+                dout.flush();
+                dout.close();
+                s.close();
+                return;
+            }
+            System.out.println("message sent");
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try{
+            Thread server = new Thread(new Server());
+            server.start();
+            Scanner in = new Scanner(System.in);
+            String message = new String();
+            while (!message.equalsIgnoreCase("over")) {
+                System.out.println("Type something to send (send over to close connection)");
+                message = in.nextLine();
+                if (message.isEmpty())
+                    continue;
+                clientcon(message);
+            }
+            System.out.println("Session End");
+
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+
+        }
     }
 }
+
+
---
+++ chandula_007/uok-messenger/src/main/java/org/uok/dscc/Server.java
@@ -0,0 +1,30 @@
+package org.uok.dscc;
+
+import java.io.DataInput;
+import java.io.DataInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server implements Runnable {
+    @Override
+    public void run() {
+        try{
+            String str;
+            ServerSocket ss = new ServerSocket(60000);
+            System.out.println("Server Started");
+
+            while (true) {
+                Socket s = ss.accept();
+                DataInputStream dis = new DataInputStream(s.getInputStream());
+                str = dis.readUTF();
+                if (str.equalsIgnoreCase("over"))
+                    break;
+                System.out.println("message = " +str);
+                dis.close();
+            }
+            ss.close();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
