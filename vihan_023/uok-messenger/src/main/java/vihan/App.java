a1105514926364ae5792e9ca18c85e094765187a
Vihan Perera <87504524+Vihan-Perera@users.noreply.github.com>
2022-03-28 04:44:16 +0530

    add single message client and server communication
    Read an input from console and save it to a variable
    Created a server socket
    Created a client socket
    Using the client socket send the message
    Read the message from server socket and print it in the console

--- vihan_023/uok-messenger/src/main/java/vihan/App.java
+++ vihan_023/uok-messenger/src/main/java/vihan/App.java
@@ -1,13 +1,31 @@
-package vihan;
+import java.util.*;
+import java.io.*;
+import java.net.*;
 
-/**
- * Hello world!
- *
- */
-public class App 
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+    	public static void main(String[] args)
+	{
+                try
+                {
+
+                	Scanner sc= new Scanner(System.in);
+                	System.out.print("Enter a message: ");
+                	String msg= sc.nextLine();
+
+                        Socket s = new Socket("127.0.0.1", 60000);
+                        DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+                        dout.writeUTF(msg);
+                        dout.flush();
+                        dout.close();
+                        s.close();
+                }
+
+                catch (Exception e)
+                {
+                        System.out.println(e);
+                }
+
+    	}
 }
