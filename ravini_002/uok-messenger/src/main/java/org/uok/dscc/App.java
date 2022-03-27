3732c70fa3983666972b4eb93f692494e59311a2
Ravini123 <arunodaj_cs17002@stu.kln.ac.lk>
2022-03-27 08:26:54 +0530

    this commit will add socket communication

--- ravini_002/uok-messenger/src/main/java/org/uok/dscc/App.java
+++ ravini_002/uok-messenger/src/main/java/org/uok/dscc/App.java
@@ -1,5 +1,9 @@
 package org.uok.dscc;
 
+import java.io.DataOutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Enter message");
+            String txt = in.nextLine();
+            Socket s = new Socket("127.0.0.1", 60000);
+            DataOutputStream dout = new
+                    DataOutputStream(s.getOutputStream());
+            dout.writeUTF(txt);
+            dout.flush();
+            dout.close();
+            s.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
     }
 }
