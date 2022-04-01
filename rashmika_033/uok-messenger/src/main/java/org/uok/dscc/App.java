3f63d86ecedc0b02836af35e9c071841231fc769
Rashmika Madushan <66150299+Rashmika9712@users.noreply.github.com>
2022-04-01 13:14:40 +0530

    Commit will add multiple messages between client and server communication
    Modify the application to client sent multiple messages.
    And read the messages from server and show them in the console.

--- rashmika_033/uok-messenger/src/main/java/org/uok/dscc/App.java
+++ rashmika_033/uok-messenger/src/main/java/org/uok/dscc/App.java
@@ -6,28 +6,34 @@ import java.net.*;
 
 public class App
 {
+
 	public static void main(String[] args)
-	{
-                try
-                {
+		{
+			Scanner scanner = new Scanner(System.in);
+                	String message = new String();
 
-                	Scanner scanner= new Scanner(System.in);
-                	System.out.print("Enter a message: ");
+                	try
+                	{
+				while(!message.equalsIgnoreCase("end"))
+				{
+                			System.out.print("Enter a message: ");
+					message =  scanner.nextLine();
 
-                	String message= scanner.nextLine();
-                        Socket skt = new Socket("127.0.0.1", 60000);
+                        		Socket skt = new Socket("127.0.0.1", 60000);
+                        		DataOutputStream dout = new DataOutputStream(skt.getOutputStream());
+                        		dout.writeUTF(message);
 
-                        DataOutputStream dout = new DataOutputStream(skt.getOutputStream());
-                        dout.writeUTF(message);
-                        dout.flush();
-                        dout.close();
-                        skt.close();
-                }
-
-                catch (Exception e)
-                {
-                        System.out.println(e);
-                }
-
-    	}
+					if(!message.equalsIgnoreCase("end"))
+					{
+                        			dout.flush();
+                        			dout.close();
+                        			skt.close();
+                			}
+				}
+			}
+	                catch (Exception e)
+        	        {
+                	        System.out.println(e);
+			}
+		}
 }
--- rashmika_033/uok-messenger/src/main/java/org/uok/dscc/Server.java
+++ rashmika_033/uok-messenger/src/main/java/org/uok/dscc/Server.java
@@ -7,22 +7,34 @@ public class Server
 {
         public static void main(String[] args)
         {
+
                 try
-                {
-                        ServerSocket serverSocket = new ServerSocket(60000);
-                        Socket skt = serverSocket.accept();
+		{
+            		ServerSocket serverSocket = new ServerSocket(60000);
+			String str = new String();
 
-                        DataInputStream dataIS = new DataInputStream(skt.getInputStream());
+            		while (!str.equalsIgnoreCase("end")) 
+			{
+                		Socket skt = serverSocket.accept();
+                		DataInputStream dataIS = new DataInputStream(skt.getInputStream());
+                		str = dataIS.readUTF();
 
-                        String str = dataIS.readUTF();
+				if (str.equalsIgnoreCase("end"))
+				{
+                   		 	break;
+				}
 
-                        System.out.println("message= " + str);
-                        serverSocket.close();
-                }
+                		System.out.println("message= " +str);
+                		dataIS.close();
+            		}
 
-                catch (Exception e)
-                {
-                        System.out.println(e);
-                }
+	            	serverSocket.close();
+        	}
+
+
+		catch (Exception e)
+		{
+            		System.out.println(e);
+        	}
         }
 }
