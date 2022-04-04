438bd13c3b421c0bd6a9d7e5d22709a3425fa0fb
chandulaed <chandulaed@gmail.com>
2022-04-04 14:39:48 +0530

    this commit will add single message client and server communication

--- chandula_007/uok-messenger/src/main/java/org/uok/dscc/App.java
+++ chandula_007/uok-messenger/src/main/java/org/uok/dscc/App.java
@@ -6,6 +6,7 @@ import java.net.Socket;
 import java.util.Scanner;
 
 
+
 public class App 
 {
 
@@ -14,14 +15,10 @@ public class App
             Socket s= new Socket("127.0.0.1", 60000);
             DataOutputStream dout = new DataOutputStream(s.getOutputStream());
             dout.writeUTF(message);
-            if (message.equalsIgnoreCase("over")) {
-                dout.flush();
-                dout.close();
-                s.close();
-                return;
-            }
+            dout.flush();
+            dout.close();
+            s.close();
             System.out.println("message sent");
-
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
@@ -30,18 +27,13 @@ public class App
     public static void main( String[] args )
     {
         try{
-
+            Thread server = new Thread(new Server());
+            server.start();
             Scanner in = new Scanner(System.in);
             String message = new String();
-            while (!message.equalsIgnoreCase("over")) {
-                System.out.println("Type something to send");
-                message = in.nextLine();
-                if (message.isEmpty())
-                    continue;
-                clientcon(message);
-            }
-            System.out.println("Session End");
-
+            System.out.println("Type something to send");
+            message = in.nextLine();
+            clientcon(message);
         }catch (Exception e){
             System.out.println(e.getMessage());
 
--- chandula_007/uok-messenger/src/main/java/org/uok/dscc/Server.java
+++ chandula_007/uok-messenger/src/main/java/org/uok/dscc/Server.java
@@ -5,26 +5,18 @@ import java.io.DataInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Server {
-    public static void main( String[] args )
-    {
-        strtserver();
-    }
-
-    public static void strtserver(){
+public class Server implements Runnable {
+    @Override
+    public void run() {
         try{
             String str;
             ServerSocket ss = new ServerSocket(60000);
             System.out.println("Server Started");
-            while (true) {
-                Socket s = ss.accept();
-                DataInputStream dis = new DataInputStream(s.getInputStream());
-                str = dis.readUTF();
-                if (str.equalsIgnoreCase("over"))
-                    break;
-                System.out.println("message = " +str);
-                dis.close();
-            }
+            Socket s = ss.accept();
+            DataInputStream dis = new DataInputStream(s.getInputStream());
+            str = dis.readUTF();
+            System.out.println("message = " +str);
+            dis.close();
             ss.close();
         }catch (Exception e){
             System.out.println(e.getMessage());
