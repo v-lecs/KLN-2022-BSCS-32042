4caa3603656f52e065d951997d72873b6b02d4ae
Vihan Perera <87504524+Vihan-Perera@users.noreply.github.com>
2022-04-06 04:54:16 +0530

    add multiple message client and server communication
    A single application was created using Java threads

--- vihan_023/uok-messenger/src/main/java/vihan/App.java
+++ vihan_023/uok-messenger/src/main/java/vihan/App.java
@@ -18,25 +18,47 @@ class Client extends Thread
 	public void run()
 	{
 
-		Scanner sc= new Scanner(System.in);
-		System.out.print("Enter a message: ");
-		String msg= sc.nextLine();
-
 		try
 		{
- 			Socket s = new Socket("127.0.0.1", 60000);
- 			DataOutputStream dout = new
-			DataOutputStream(s.getOutputStream());
- 			dout.writeUTF(msg);
- 			dout.flush();
- 			dout.close();
- 			s.close();
-		}
+			Scanner sc = new Scanner(System.in);
+			String msg = new String();
+
+			System.out.println("Enter messages: ");
+
+			while (!msg.equalsIgnoreCase("exit"))
+			{
+                		//System.out.println("Enter a message: ");
+                		msg = sc.nextLine();
+
+				try
+				{
+            				Socket s= new Socket("127.0.0.1", 60000);
+            				DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+            				dout.writeUTF(msg);
+            				if (msg.equalsIgnoreCase("exit"))
+					{
+                				dout.flush();
+                				dout.close();
+                				s.close();
+            				}
+
+        			}
+
+			        catch (Exception e)
+				{
+            				System.out.println(e.getMessage());
+                		}
+
+            		}
+
+        	}
+
 
 		catch (Exception e)
 		{
- 			System.out.println(e);
-		}
+            		System.out.println(e.getMessage());
+
+        	}
         }
 
 
@@ -46,19 +68,31 @@ class Server extends Thread
 {
         public void run()
         {
-                try
-                {
-                        ServerSocket ss = new ServerSocket(60000);
-                        Socket s = ss.accept();
-                        DataInputStream dis = new DataInputStream(s.getInputStream());
-                        String str = dis.readUTF();
-                        System.out.println("message= " + str);
-                        ss.close();
-                }
+		try
+		{
+            		ServerSocket ss = new ServerSocket(60000);
 
-                catch (Exception e)
-                {
-                        System.out.println(e);
-                }
+            		while (true)
+			{
+                		Socket s = ss.accept();
+                		DataInputStream dis = new DataInputStream(s.getInputStream());
+                		String str = dis.readUTF();
+
+				if (str.equalsIgnoreCase("exit"))
+				{
+                   	 		break;
+				}
+
+                		System.out.println("message = " +str);
+                		dis.close();
+            		}
+
+	            		ss.close();
+        	}
+
+		catch (Exception e)
+		{
+            		System.out.println(e.getMessage());
+        	}
         }
 }
