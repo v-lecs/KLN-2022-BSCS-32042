b86fcc3819bf0d68b35f01c86a36329d71cf71ba
Vihan Perera <87504524+Vihan-Perera@users.noreply.github.com>
2022-04-06 04:49:53 +0530

    add single message client and server communication
    A single application was created using Java threads

--- vihan_023/uok-messenger/src/main/java/vihan/App.java
+++ vihan_023/uok-messenger/src/main/java/vihan/App.java
@@ -1,50 +1,64 @@
+package vihan;
+
 import java.util.*;
 import java.io.*;
 import java.net.*;
 
 public class App
 {
-
-	public static void main( String[] args )
+	public static void main(String[] args)
 	{
-       		try
+		new Server().start();
+		new Client().start();
+	}
+}
+
+class Client extends Thread
+{
+	public void run()
+	{
+
+		Scanner sc= new Scanner(System.in);
+		System.out.print("Enter a message: ");
+		String msg= sc.nextLine();
+
+		try
 		{
-			Scanner sc = new Scanner(System.in);
-			String msg = new String();
-
-			while (!msg.equalsIgnoreCase("exit"))
-			{
-                		System.out.println("Enter a message: ");
-                		msg = sc.nextLine();
-
-				try
-				{
-            				Socket s= new Socket("127.0.0.1", 60000);
-            				DataOutputStream dout = new DataOutputStream(s.getOutputStream());
-            				dout.writeUTF(msg);
-            				if (msg.equalsIgnoreCase("exit"))
-					{
-                				dout.flush();
-                				dout.close();
-                				s.close();
-            				}
-
-        			}
-
-			        catch (Exception e)
-				{
-            				System.out.println(e.getMessage());
-                		}
-
-            		}
-
-        	}
-
+ 			Socket s = new Socket("127.0.0.1", 60000);
+ 			DataOutputStream dout = new
+			DataOutputStream(s.getOutputStream());
+ 			dout.writeUTF(msg);
+ 			dout.flush();
+ 			dout.close();
+ 			s.close();
+		}
 
 		catch (Exception e)
 		{
-            		System.out.println(e.getMessage());
+ 			System.out.println(e);
+		}
+        }
 
-        	}
-	}
+
+}
+
+class Server extends Thread
+{
+        public void run()
+        {
+                try
+                {
+                        ServerSocket ss = new ServerSocket(60000);
+                        Socket s = ss.accept();
+                        DataInputStream dis = new DataInputStream(s.getInputStream());
+                        String str = dis.readUTF();
+                        System.out.println("message= " + str);
+                        ss.close();
+                }
+
+                catch (Exception e)
+                {
+                        System.out.println(e);
+                }
+        }
 }
