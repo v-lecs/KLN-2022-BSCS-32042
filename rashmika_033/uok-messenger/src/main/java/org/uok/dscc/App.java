fb412a05d0a42e4af53bd980dd28d4158b3c6d7d
Rashmika Madushan <66150299+Rashmika9712@users.noreply.github.com>
2022-04-08 10:07:53 +0530

    Commit will add single message between client and server communication
    Modify the application to client sent multiple messages.
    And read the messages from server and show them in the console.

--- rashmika_033/uok-messenger/src/main/java/org/uok/dscc/App.java
+++ rashmika_033/uok-messenger/src/main/java/org/uok/dscc/App.java
@@ -15,33 +15,39 @@ public class App
 
 }
 
-
 class Client extends Thread
 {
-		public void run()
-		{
-                	try
-                	{
+	public void run()
+	{
+		Scanner scanner= new Scanner(System.in);
+                String message = new String();
 
-        	        	Scanner scanner= new Scanner(System.in);
-                		System.out.print("Enter a message: ");
+		System.out.print("Enter your message: ");
 
-                		String message= scanner.nextLine();
-                   		Socket skt = new Socket("127.0.0.1", 60000);
+                try
+                {
+			while(!message.equalsIgnoreCase("stop"))
+			{
+                        	message =  scanner.nextLine(); 
+                        	Socket skt = new Socket("127.0.0.1", 50000);
 
-                        	DataOutputStream dout = new DataOutputStream(skt.getOutputStream());
+                        	 DataOutputStream dout = new DataOutputStream(skt.getOutputStream());
                         	dout.writeUTF(message);
-                        	dout.flush();
-                        	dout.close();
-                        	skt.close();
-                	}
 
-                	catch (Exception e)
-                	{
-                        	System.out.println(e);
-                	}
+				if(!message.equalsIgnoreCase("stop"))
+				{
+                		        dout.flush();
+                        		dout.close();
+                        		skt.close();
+               			 }
+			}
+		}
+                catch (Exception e)
+                {
+                        System.out.println(e);
+                }
 
-    		}
+    	}
 }
 
 class Server extends Thread
@@ -49,20 +55,35 @@ class Server extends Thread
 	public void run()
 	{
 		try
-                {
-                        ServerSocket serverSocket = new ServerSocket(60000);
-                        Socket skt = serverSocket.accept();
+		{
+            		ServerSocket serverSocket = new ServerSocket(50000);
+			String strng = new String();
 
-                        DataInputStream dis = new DataInputStream(skt.getInputStream());
-                        String strng = dis.readUTF();
+            		while (!strng.equalsIgnoreCase("stop")) 
+			{
+                		Socket skt = serverSocket.accept();
+                		DataInputStream dis = new DataInputStream(skt.getInputStream());
+                		strng = dis.readUTF();
 
-                        System.out.println("message= " + strng);
-                        serverSocket.close();
-                }
+				if (strng.equalsIgnoreCase("stop"))
+				{
+                   	 		break;
+				}
 
-                catch (Exception e)
-                {
-                        System.out.println(e);
-                }
-        }
+                		System.out.println("message= " +strng);
+				System.out.print("\n");
+				System.out.print("Enter your message: ");
+                		dis.close();
+            		}
+
+	            	serverSocket.close();
+        	}
+
+
+		catch (Exception e)
+		{
+            		System.out.println(e);
+        	}
+    	}
 }
+
