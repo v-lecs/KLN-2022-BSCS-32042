eeeee8252eadae30a614ec51fc18ebfb584e00ae
iumayanga <101942307+iumayanga@users.noreply.github.com>
2022-04-08 17:31:42 +0530

    This commit will add single message client and server communication.
    Tasks :
    Read an input from console and save it to a variable.
    Create a server socket and  a client socket.
    Using the client socket send a message.
    Read the message from server socket and print it in the console.

--- isuru_047/uok-messenger/src/main/java/org/uok/dscc/App.java
+++ isuru_047/uok-messenger/src/main/java/org/uok/dscc/App.java
@@ -1,52 +1,64 @@
 package org.uok.dscc;
-
 import java.util.*;
 import java.io.*;
 import java.net.*;
 
+
 public class App
 {
 
-	public static void main( String[] args )
+    	public static void main(String[] args)
 	{
-       		try
-		{
-			Scanner sc1 = new Scanner(System.in);
-			String msg1 = new String();
-
-			while (!msg1.equalsIgnoreCase("exit"))
-			{
-                		System.out.println("Enter a message: ");
-                		msg1 = sc1.nextLine();
-
-				try
-				{
-            				Socket soc1= new Socket("127.0.0.1", 60000);
-            				DataOutputStream dout = new DataOutputStream(soc1.getOutputStream());
-            				dout.writeUTF(msg1);
-            				if (msg1.equalsIgnoreCase("exit"))
-					{
-                				dout.flush();
-                				dout.close();
-                				soc1.close();
-            				}
-
-        			}
-
-			        catch (Exception e)
-				{
-            				System.out.println(e.getMessage());
-                		}
-
-            		}
-
-        	}
-
-
-		catch (Exception e)
-		{
-            		System.out.println(e.getMessage());
-
-        	}
+		new Server().start();
+		new Client().start();
 	}
+
+}
+
+class Client extends Thread
+{
+		public void run()
+		{
+                	try
+                	{
+
+        	        	Scanner scan= new Scanner(System.in);
+                		System.out.print("Enter the message: ");
+                		String message= scan.nextLine();
+                   		Socket soc = new Socket("127.0.0.1", 60000);
+                        	DataOutputStream dout = new DataOutputStream(soc.getOutputStream());
+                        	dout.writeUTF(message);
+                        	dout.flush();
+                        	dout.close();
+                        	soc.close();
+                	}
+
+                	catch (Exception e)
+                	{
+                        	System.out.println(e);
+                	}
+
+    		}
+}
+
+class Server extends Thread
+{
+	public void run()
+	{
+		try
+                {
+                        ServerSocket ssoc = new ServerSocket(60000);
+                        Socket soc = ssoc.accept();
+                        DataInputStream din = new DataInputStream(soc.getInputStream());
+                        String str = din.readUTF();
+                        System.out.println("message= " + str);
+                        ssoc.close();
+                }
+
+                catch (Exception e)
+                {
+                        System.out.println(e);
+                }
+
+        }
 }
