446f962b779d77f804fb28a664bcc8581a2f8c60
chandulaed <chandulaed@gmail.com>
2022-05-30 00:24:11 +0530

    Add and Initialize firebase SDK

--- chandula_007/uok-messenger/src/main/java/org/uok/dscc/App.java
+++ chandula_007/uok-messenger/src/main/java/org/uok/dscc/App.java
@@ -1,4 +1,13 @@
 package org.uok.dscc;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
@@ -9,7 +18,15 @@ import java.util.Scanner;
 public class App
 {
 
-        public static void main (String[]args ) throws NotBoundException, RemoteException {
+        public static void main (String[]args ) throws NotBoundException, IOException {
+            //Initialize firbase admin sdk
+            FileInputStream serviceaccount = new FileInputStream("./src/main/resources/firebase_credentials.json");
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceaccount))
+                    .setDatabaseUrl("https://uok-messenger-cs007-default-rtdb.firebaseio.com/")
+                    .build();
+            FirebaseApp.initializeApp(options);
+
             //Create a server and client classes and start server
             Thread server = new Thread(new Server());
             server.start();
